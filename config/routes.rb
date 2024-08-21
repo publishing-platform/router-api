6d99a321ec64b9ef5a835d65b108f3aa07228733
neilha <neilharrold@gmail.com>
2024-08-21 12:42:42 +0100

    adds routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  with_options format: false do |r|
+    r.resources :backends, only: %i[show update destroy]
+    
+    r.get "/routes" => "routes#show"
+    r.put "/routes" => "routes#update"
+    r.delete "/routes" => "routes#destroy"
+  end
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
